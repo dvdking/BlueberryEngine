a608fbeba02196c91f90e5e5d304bc889a357937
dvdking <dvdking@i.ua>
2013-12-03 16:07:06 +0200

    post effect mgr basic classes

---
+++ PostEffects/PostEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blueberry.PostEffects
+{
+    class PostEffect
+    {
+        internal string Name;
+    }
+}
---
+++ PostEffects/PostEffectMgr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blueberry.PostEffects
+{
+    public static class PostEffectMgr
+    {
+        static List<PostEffect> _currentPostEffects = new List<PostEffect>();
+        static List<PostEffect> _loadedPostEffects = new List<PostEffect>();
+
+        public static void AddEffect(string name)
+        {
+            PostEffect p = _loadedPostEffects.Find(t => t.Name == name);
+            _currentPostEffects.Add(p);
+        }
+
+        public static void ResetActiveEffects()
+        {
+            _currentPostEffects.Clear();
+        }
+
+        static internal void PostEffectBegin()
+        {
+ 
+        }
+
+        static internal void PostEffectEnd()
+        {
+ 
+        }
+    }
+}
